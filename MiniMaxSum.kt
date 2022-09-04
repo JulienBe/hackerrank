957a3f39918b35093db3b52ecc415f282efc5037
Bertozzi Julien <bertozzijulien@gmail.com>
2022-09-04 20:25:16 +0200

    Create MiniMaxSum.kt

---
+++ MiniMaxSum.kt
@@ -0,0 +1,38 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun miniMaxSum(arr: Array<Int>): Unit {
+    // Write your code here
+    arr.sort()
+    val sum: Long = arr.fold(0L) { acc, next -> acc + next.toLong() }
+    println("${sum - arr[arr.size - 1].toLong()} ${sum - arr[0].toLong()}")
+}
+
+fun main(args: Array<String>) {
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    miniMaxSum(arr)
+}
